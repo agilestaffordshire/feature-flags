615d78c393d7e94c5c10c6ee9d4396dc3c11a980
Paul Williams <paul@paulswilliams.me.uk>
2015-05-24 10:37:25 +0100

    Moved controller into attendees package

--- src/main/java/org/agilestaffordshire/featureflags/attendees/AttendeesController.java
+++ src/main/java/org/agilestaffordshire/featureflags/attendees/AttendeesController.java
@@ -1,10 +1,11 @@
-package org.agilestaffordshire.featureflags;
+package org.agilestaffordshire.featureflags.attendees;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AttendeesController {
+
 
     @RequestMapping("/")
     public String index() {
