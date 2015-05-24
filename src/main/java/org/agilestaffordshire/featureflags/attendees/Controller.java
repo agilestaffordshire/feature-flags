c3584f9e7fc7079f9c59b7ef2927880baa33d948
Paul Williams <paul@paulswilliams.me.uk>
2015-05-24 11:12:08 +0100

    Listing attendees

--- src/main/java/org/agilestaffordshire/featureflags/attendees/Controller.java
+++ src/main/java/org/agilestaffordshire/featureflags/attendees/Controller.java
@@ -4,12 +4,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class Controller {
 
 
     @RequestMapping("/")
     public ModelAndView index() {
-        return new ModelAndView("list", null);
+        List<Attendee> fakeAttendees = new ArrayList<>();
+        Attendee paul = new Attendee("Paul", "Williams");
+        fakeAttendees.add(paul);
+        return new ModelAndView("list", "attendees", fakeAttendees);
     }
 }
