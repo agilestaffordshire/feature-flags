ec27fc62db77ed7d473e58733d0de24beae70e44
Paul Williams <paul@paulswilliams.me.uk>
2015-05-24 10:55:31 +0100

    Basic templating support

--- src/main/java/org/agilestaffordshire/featureflags/attendees/Controller.java
+++ src/main/java/org/agilestaffordshire/featureflags/attendees/Controller.java
@@ -2,13 +2,14 @@ package org.agilestaffordshire.featureflags.attendees;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class Controller {
 
 
     @RequestMapping("/")
-    public String index() {
-        return "hello";
+    public ModelAndView index() {
+        return new ModelAndView("list", null);
     }
 }
