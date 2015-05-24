1aa2ccae187eee31b22bdeffc8b6e761e26d4379
Paul Williams <paul@paulswilliams.me.uk>
2015-05-24 11:11:43 +0100

    Introduced attendee

---
+++ src/main/java/org/agilestaffordshire/featureflags/attendees/Attendee.java
@@ -0,0 +1,11 @@
+package org.agilestaffordshire.featureflags.attendees;
+
+public class Attendee {
+    public final String firstname;
+    public final String secondname;
+
+    public Attendee(String firstname, String secondname) {
+        this.firstname = firstname;
+        this.secondname = secondname;
+    }
+}
